b563bdac9d7c4bbd954a6910a3eb8d9b9365c347
victoriastruk <struk.victoria@gmail.com>
2023-07-09 02:41:31 +0300

    dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <header className = "App-header" >
+    <div className = "nav-area">
+      <a href="#/" className = "logo">Codesolution</a>
+      <Navbar/>
     </div>
+    </header>
   );
-}
+};
 
 export default App;
---
+++ src/menuItems.js
@@ -0,0 +1,35 @@
+export const menuItems = [ {
+   title: "Home",
+}, {
+   title: "Services",
+   submenu: [ {
+         title: "Web design"
+      },
+      {
+         title: "Web development",
+         submenu: [ {
+               title: "Frontend"
+            },
+            {
+               title: "Backend",
+               submenu: [ {
+                  title: "Node.js",
+               }, {
+                  title: "PHP"
+               } ]
+            }
+         ]
+      },{
+         title: "SEO",
+      },
+   ]
+},{
+   title: "About US",
+   submenu:[
+      {
+         title:"Who we are"
+      },{
+         title: "Our Values",
+      },
+   ]
+}, ]
